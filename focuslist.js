c3dab410ae71c3c54bc5e7ef6ecfc02dac001abc
mattgd <mattdzwonczyk@gmail.com>
2016-08-28 22:12:30 -0400

    Started styling list for popup panel.

--- focuslist.js
+++ focuslist.js
@@ -1,10 +1,11 @@
-var key = "";
+chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    for (var i = 0; i < allKeys.length; i++) {
+      if (!allKeys[i].startsWith("http"))
+        array.splice(i, 1);
+    }
 
-console.log(localStorage);
-console.log("STORAGE:" + localStorage.length);
-
-for (var i = 0; i < localStorage.length; i++) {
-  console.log(i);
-  key = localStorage.key(i);
-  $('body').append('<div class="focus-website"><p>' + key + '</p></div>');
-}
+    for (var i = 0; i < allKeys.length; i++) {
+      $('body').append('<div class="focus-website"><button type="button" class="set-focus-button remove">X</button><p><a target="_blank" href="' + allKeys[i] + '">' + allKeys[i].substr(0, 16) + '</a></p></div>');
+    }
+});
