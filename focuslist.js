8dd05f12138a74e7efca21b37e4c052d9948cfa3
mattgd <mattdzwonczyk@gmail.com>
2016-09-03 17:11:58 -0400

    Remove the table if there are no websites saved.

--- focuslist.js
+++ focuslist.js
@@ -6,7 +6,7 @@ chrome.storage.sync.get(null, function(items) {
     }
 
     if (allKeys.length > 0) {
-      var focusTable = '<div class="set-focus-table"><table class="table table-striped"><tr><th>Remove</th><th>URL</th></tr>';
+      var focusTable = '<div class="set-focus-table"><table class="table table-striped" id="focus-list"><tr><th>Remove</th><th>URL</th></tr>';
 
       for (var i = 0; i < allKeys.length; i++) {
         focusTable += '<tr><td><button type="button" class="btn btn-danger" id="remove-button">X</button></td><td><p><a target="_blank" href="' + allKeys[i] + '">' + allKeys[i].substr(0) + '</a></p></td></tr>';
@@ -19,7 +19,8 @@ chrome.storage.sync.get(null, function(items) {
         var row = $(this).parent().parent();
         var url = row.find("a").attr('href');
 
-        row.hide("slow");
+        row.remove();
+        if ($('#focus-list tr').length <= 1) $('#focus-list').hide();
 
         // Remove the value from Chrome's sync storage
         chrome.storage.sync.remove(url, function(){
