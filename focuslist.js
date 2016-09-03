2d18328521e9007ca6e52a2ef7795e62d2c72642
mattgd <mattdzwonczyk@gmail.com>
2016-09-03 17:00:14 -0400

    Website list now displays in a table.

--- focuslist.js
+++ focuslist.js
@@ -5,7 +5,26 @@ chrome.storage.sync.get(null, function(items) {
         array.splice(i, 1);
     }
 
-    for (var i = 0; i < allKeys.length; i++) {
-      $('body').append('<div class="focus-website"><button type="button" class="set-focus-button remove">X</button><p><a target="_blank" href="' + allKeys[i] + '">' + allKeys[i].substr(0, 16) + '</a></p></div>');
+    if (allKeys.length > 0) {
+      var focusTable = '<div class="set-focus-table"><table class="table table-striped"><tr><th>Remove</th><th>URL</th></tr>';
+
+      for (var i = 0; i < allKeys.length; i++) {
+        focusTable += '<tr><td><button type="button" class="btn btn-danger" id="remove-button">X</button></td><td><p><a target="_blank" href="' + allKeys[i] + '">' + allKeys[i].substr(0) + '</a></p></td></tr>';
+      }
+
+      focusTable += '</table></div>';
+      $('body').append(focusTable);
+
+      $("#remove-button").click(function() {
+        var row = $(this).parent().parent();
+        var url = row.find("a").attr('href');
+
+        row.hide("slow");
+
+        // Remove the value from Chrome's sync storage
+        chrome.storage.sync.remove(url, function(){
+          console.log("Focus element removed for " + url);
+        });
+      });
     }
 });
